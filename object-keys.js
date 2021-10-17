4057ce777ab09e94640619233c672410c5ad94b2
Russell M <russell.e.mair@gmail.com>
2021-10-17 08:31:47 +0000

    all javascripting up to functions 1

---
+++ object-keys.js
@@ -0,0 +1,9 @@
+const car = {  
+  make: 'Honda',  
+  model: 'Accord',  
+  year: 2020  
+}  
+
+const keys = Object.keys(car)  
+console.log(keys)
+//this code makes a string array that has every key in the object. this makes it easy to sort through them.
